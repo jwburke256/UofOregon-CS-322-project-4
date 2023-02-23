b3c1281fa9b1fa4ff287dbda69e56fbce7bd9851
jwburke256 <jwburke256@gmail.com>
2023-02-23 01:58:23 -0800

    adjusted control times over brevit times for open_times()

--- brevets/acp_times.py
+++ brevets/acp_times.py
@@ -18,6 +18,7 @@ import arrow
 maxSpeeds = [(200,34), (200,32), (200,30), (400,28), (300,26)]
 minSpeeds = [(600,15), (400,11.428), (300,13.33)]
 
+brevetStartTimes = {200:5.53, 300:9, 400:12.08, 600:18.48, 1000:33.05 }
 brevetEndTimes = {200:13.5, 300:20, 400:27, 600:40, 1000:75, 1200:90, 1400:116.4, 2200:220}
 
 def open_time(control_dist_km, brevet_dist_km, brevet_start_time):
@@ -34,6 +35,15 @@ def open_time(control_dist_km, brevet_dist_km, brevet_start_time):
     """
     
     if control_dist_km == 0:
+        return brevet_start_time
+    if control_dist_km >= brevet_dist_km:
+        return_time = brevetStartTimes[brevet_dist_km]
+        hours = return_time
+        hours = int(hours)
+        mins = return_time - hours
+        mins = mins * 100
+        brevet_start_time = brevet_start_time.shift(hours = hours) 
+        brevet_start_time = brevet_start_time.shift(minutes = mins) 
         return brevet_start_time
     for speeds in maxSpeeds:
         dist, time = speeds
--- brevets/tests/test_acp_times.py
+++ brevets/tests/test_acp_times.py
@@ -30,4 +30,32 @@ def test_brevet1():
             }
     check_brevets(checkpoints, dist, start_time)
 
+def test_brevet2():
+    start_time = arrow.get("2023-02-22 00:00", "YYYY-MM-DD HH:mm")
+    dist = 200
+    checkpoints = {
+        0: (start_time, start_time.shift(hours=1)),
+        60: (start_time.shift(hours=1, minutes=46), start_time.shift(hours=4)),
+        120: (start_time.shift(hours=3, minutes=32), start_time.shift(hours=8)),
+        175: (start_time.shift(hours=5, minutes=9), start_time.shift(hours=11, minutes=40)),
+        205: (start_time.shift(hours=5, minutes=53), start_time.shift(hours=13.5)), 
+            }
+    check_brevets(checkpoints, dist, start_time)
+
+def test_brevet3():
+    start_time = arrow.get("2023-02-22 00:00", "YYYY-MM-DD HH:mm")
+    dist = 400
+    checkpoints = {
+        0: (start_time, start_time.shift(hours=1)),
+        50: (start_time.shift(hours=1, minutes=28), start_time.shift(hours=3.5)),
+        100: (start_time.shift(hours=2, minutes=56), start_time.shift(hours=6, minutes=40)),
+        150: (start_time.shift(hours=4, minutes=25), start_time.shift(hours=10)),
+        200: (start_time.shift(hours=5, minutes=53), start_time.shift(hours=13, minutes=20)),
+        250: (start_time.shift(hours=7, minutes=27), start_time.shift(hours=16, minutes=40)),
+        300: (start_time.shift(hours=9, minutes=1), start_time.shift(hours=20)),
+        350: (start_time.shift(hours=10, minutes=34), start_time.shift(hours=23, minutes=20)),
+        400: (start_time.shift(hours=12, minutes=8), start_time.shift(days=1, hours=3)),
+            }
+    check_brevets(checkpoints, dist, start_time)
+
 
